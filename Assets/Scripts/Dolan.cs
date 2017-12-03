cd8454f67ef2ee420cb200f8a7241ffc62ab0e70
Inconnu08 <taufiqrx8@gmail.com>
2017-12-04 01:41:52 +0600

    feat: audio

---
+++ Assets/Scripts/Dolan.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dolan : MonoBehaviour {
+
+	public float upForce = 200f;
+<<<<<<< HEAD
+	public AudioSource[] sound;
+	public AudioSource flapSound;
+	public AudioSource hit;
+=======
+>>>>>>> origin/master
+
+	private bool isDead = false;
+	private Rigidbody2D rb2d;
+	private Animator anime;
+
+	void Start () {
+		rb2d  = GetComponent<Rigidbody2D> ();
+		anime = GetComponent<Animator> ();
+<<<<<<< HEAD
+		sound = GetComponents<AudioSource> ();
+		flapSound = sound[0];
+		hit = sound[1];
+=======
+>>>>>>> origin/master
+	}
+
+	void Update () {
+		if (isDead == false) {
+			if (Input.GetMouseButtonDown(0)) {
+				rb2d.velocity = Vector2.zero;
+				rb2d.AddForce (new Vector2(0, upForce));
+				anime.SetTrigger ("Flap");
+<<<<<<< HEAD
+				flapSound.Play ();
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.Escape)) { 
+			Application.Quit (); 
+		}
+	}
+
+	void OnCollisionEnter2D () {
+		rb2d.velocity = Vector2.zero;
+		isDead = true;
+		anime.SetTrigger ("Die");
+		hit.Play ();
+		GameControl.instance.DolanDied();
+=======
+
+			}
+		}
+	}
+
+	void OnCollisionEnter2D () {
+		isDead = true;
+		anime.SetTrigger ("Die");
+>>>>>>> origin/master
+	}
+}
---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+
+	public static GameControl instance;
+	public GameObject gameOverText;
+	public Text scoreText;
+	public Text highScore;
+	public bool gameOver = false;
+	public float scrollSpeed = -1.5f;
+
+	private int score = 0;
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		}else if (instance != this) {
+			Destroy (gameObject);
+		}
+	}
+
+	void Start () {
+		highScore.text = "High Score: " + PlayerPrefs.GetInt ("HighScore", 0).ToString();
+	}
+
+	void Update () {
+		if (gameOver == true && Input.GetMouseButtonDown(0)) {
+			// reload the current scene
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+		}
+	}
+
+	public void BirdScore() {
+		if (gameOver) {
+			return;
+		}
+		score++;
+		scoreText.text = "Score: " + score.ToString();
+
+		if (score > PlayerPrefs.GetInt ("HighScore", 0)) {
+			PlayerPrefs.SetInt ("HighScore", score);
+			highScore.text = "High Score: " + score.ToString();
+		}
+	}
+
+	public void DolanDied() {
+		gameOverText.SetActive(true);
+		gameOver = true;
+	}
+}
